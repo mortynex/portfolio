0cb5d22f253f2b3181a21bdeef2d29bf1f52f67f
Martin Polťák <46408007+mortynex@users.noreply.github.com>
2024-04-09 23:13:30 +0200

    feat: parallax & glob animations

--- smoothScroll.js
+++ smoothScroll.js
@@ -1,27 +1,51 @@
 const smoothScrollElements = [
-	...document.querySelectorAll("*[data-scroll-speed]"),
+	...document.querySelectorAll(
+		"*[data-scroll-speed-top], *[data-scroll-speed-left], *[data-scroll-speed-right]"
+	),
 ];
 
-const defaultTopValues = new Map();
+const defaultStyleValues = new Map();
 
 window.addEventListener("load", () => {
 	for (const elem of smoothScrollElements) {
 		const style = window.getComputedStyle(elem);
 
-		defaultTopValues.set(elem, style.top);
+		const getValue = (value) => (!value.endsWith("px") ? "0px" : value);
+
+		defaultStyleValues.set(elem, {
+			top: getValue(style.top),
+			left: getValue(style.left),
+			right: getValue(style.right),
+		});
+
+		if (style.position === "static") {
+			elem.style.position = "relative";
+		}
 	}
 
-	console.log(defaultTopValues);
+	console.log(defaultStyleValues);
 });
 
 document.body.addEventListener("scroll", () => {
-	console.log(defaultTopValues);
-
 	for (const elem of smoothScrollElements) {
-		const { scrollSpeed } = elem.dataset;
+		const { scrollSpeedTop, scrollSpeedRight, scrollSpeedLeft } = elem.dataset;
 
-		elem.style.top = `calc(${defaultTopValues.get(elem)} + ${
-			document.body.scrollTop / scrollSpeed
-		}px)`;
+		if (scrollSpeedTop) {
+			elem.style.top = `calc(${defaultStyleValues.get(elem).top} + ${
+				document.body.scrollTop / scrollSpeedTop
+			}px)`;
+		}
+
+		if (scrollSpeedRight) {
+			elem.style.left = `calc(${defaultStyleValues.get(elem).right} + ${
+				document.body.scrollTop / scrollSpeedRight
+			}px)`;
+		}
+
+		if (scrollSpeedLeft) {
+			elem.style.left = `calc(${defaultStyleValues.get(elem).left} + ${
+				document.body.scrollTop / scrollSpeedLeft
+			}px)`;
+		}
 	}
 });
