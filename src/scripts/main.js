2bf07b775178d4b926ad72760c503b22904b966b
Martin Polťák <46408007+mortynex@users.noreply.github.com>
2024-04-17 10:59:16 +0200

    fix: flower hover effect

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,18 @@ import gsap from "gsap";
 
 let currentRotation = 0;
 
-document.querySelector(".flower").addEventListener("click", () => {
-	gsap.to(".flower", {
-		duration: 1,
-		rotation: (currentRotation += 360),
-		ease: "elastic.out(1, 0.3)",
-	});
+const flowerElement = document.querySelector(".flower");
+
+flowerElement.addEventListener("click", () => {
+	gsap
+		.to(".flower", {
+			duration: 1,
+			rotation: (currentRotation += 360),
+			ease: "elastic.out(1, 0.3)",
+		})
+		.then(() => {
+			setTimeout(() => {
+				flowerElement.style.scale = "";
+			}, 1000);
+		});
 });
