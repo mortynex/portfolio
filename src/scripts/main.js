9bf81510b0856712f7e7820cd075114b5f7cd11f
Martin Polťák <46408007+mortynex@users.noreply.github.com>
2024-05-11 18:45:04 +0200

    feat: makacka

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,20 @@
 import "./smoothScroll.js";
+import "./randomLinks.js";
+
 import gsap from "gsap";
 
 let currentRotation = 0;
 
 const flowerElement = document.querySelector(".flower");
+
+let flowerClickTimes = 0;
+let currentHue = 292;
+
+const root = document.querySelector(":root");
+
+console.log({
+	value: root.style.getPropertyValue("--primary-color"),
+});
 
 flowerElement.addEventListener("click", () => {
 	gsap
@@ -17,4 +28,108 @@ flowerElement.addEventListener("click", () => {
 				flowerElement.style.scale = "";
 			}, 1000);
 		});
+
+	flowerClickTimes++;
+
+	if (flowerClickTimes == 10) {
+		setInterval(() => {
+			currentHue++;
+
+			if (currentHue > 360) currentHue = 0;
+
+			root.style.setProperty(
+				"--secondary-color",
+				`hsl(${currentHue}, 77%, 74%)`
+			);
+
+			console.log({
+				value: getComputedStyle(root).getPropertyValue("--secondary-color"),
+				e: `hsl(${currentHue}, 77%, 74%);`,
+			});
+		}, 50);
+	}
+});
+
+const triggers = document.querySelector(".aboutMeTitle img");
+const target = document.querySelector(".aboutMeTitle");
+
+triggers.addEventListener("click", () => {
+	target.classList.toggle("upsideDown");
+});
+
+const icons = document.querySelectorAll(".textIcon");
+
+const currentScales = new Map();
+
+icons.forEach((icon) => {
+	icon.addEventListener("click", () => {
+		let currentScale = currentScales.get(icon) ?? 1.0;
+
+		currentScale += 0.09;
+
+		currentScales.set(icon, currentScale);
+
+		icon.style.transform = `scale(${currentScale})`;
+
+		if (currentScale > 3) {
+			icon.animate([{ transform: "scale(0.01)", visibility: "hidden" }], {
+				duration: 300,
+				easing: "ease-in-out",
+				fill: "forwards",
+				iterations: 1,
+			});
+		}
+	});
+});
+
+const rocket = document.querySelector("#flyingRocket");
+
+let rocketClickedTimes = 0;
+
+rocket.addEventListener("click", () => {
+	rocketClickedTimes++;
+
+	if (rocketClickedTimes > 3) return;
+
+	if (rocketClickedTimes === 3) {
+		// out animation
+
+		return rocket
+			.animate([{ transform: "translate(100vh, -100vw) scale(25)" }], {
+				duration: 6000,
+				easing: "ease-in-out",
+				fill: "forwards",
+				iterations: 1,
+			})
+			.addEventListener("finish", () => {
+				rocketClickedTimes = 0;
+			});
+	}
+
+	// rotating rocking animation
+
+	rocket.animate(
+		[
+			{ transform: "translate(0px, 0px)" },
+
+			{ transform: "translate(-3px, -3px) rotate(-2deg)" },
+			{ transform: "translate(3px, 3px) rotate(2deg)" },
+
+			{ transform: "translate(0px, 0px)" },
+		],
+		{
+			duration: 1000,
+			iterations: 1,
+		}
+	);
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior: "smooth",
+		});
+	});
 });
---
+++ src/scripts/randomLinks.js
@@ -0,0 +1,26 @@
+const LINK_TEXTS = [
+	"👉 click me!",
+	"visit 🚗",
+	"inspect 🧐",
+	"🌊 sea more",
+	"explore 🌍",
+	"discover 🕵️‍♂️",
+	"🔍 find out",
+	"learn more 🤓",
+	"take a look 🫣",
+	"peek inside 👀",
+	"peek 👀",
+	"check out 🛒",
+];
+const generateRandomLinkText = (prev) => {
+	const randomLink = LINK_TEXTS[Math.floor(Math.random() * LINK_TEXTS.length)];
+
+	return randomLink === prev ? generateRandomLinkText(prev) : randomLink;
+};
+document.querySelectorAll("*[data-generate-link-text]").forEach((link) => {
+	link.textContent = generateRandomLinkText();
+
+	link.parentElement.addEventListener("mouseenter", () => {
+		link.textContent = generateRandomLinkText(link.textContent);
+	});
+});
